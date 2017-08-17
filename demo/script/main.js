255c9ffc042f1f51b2e34c9c8d60ed409806b714
Nam <namhoang.td09@gmail.com>
2017-08-17 16:17:43 +0700

    Add socket client

--- demo/script/main.js
+++ demo/script/main.js
@@ -1,38 +1,65 @@
 // Add your javascript here
 
+
+const socket = io('http://localhost:3000');
+
+
 var langTools = ace.require("ace/ext/language_tools");
-    var editor = ace.edit("editor");
-    // editor.setTheme("ace/theme/twilight");
+var editor = ace.edit("editor");
+// editor.setTheme("ace/theme/twilight");
 
 
 
-    editor.setOptions({enableBasicAutocompletion: true});
-    document.getElementById('editor').style.fontSize = '16px';
-    // uses http://rhymebrain.com/api.html
-    var rhymeCompleter = {
-        getCompletions: function(editor, session, pos, prefix, callback) {
-            if (prefix.length === 0) { callback(null, []); return }
-            console.log(prefix)
-
-
-            $.getJSON(
-                "http://bar:3000/suggestion/" + prefix,
-                function(wordList) {
-                    console.log(wordList)
-                    // wordList like [{"word":"flow","freq":24,"score":300,"flags":"bc","syllables":"1"}]
-                    callback(null, wordList.map(function(ea) {
-                        return {name: ea.Word, value: ea.Word, score: ea.Count, meta: "tech"}
-                    }));
-                })
+editor.setOptions({
+    enableBasicAutocompletion: true
+});
+document.getElementById('editor').style.fontSize = '16px';
+// uses http://rhymebrain.com/api.html
+var rhymeCompleter = {
+    getCompletions: function(editor, session, pos, prefix, callback) {
+        if (prefix.length === 0) {
+            callback(null, []);
+            return
         }
+        console.log(prefix)
+
+        socket.emit('suggestion', prefix)
+        socket.on('suggestion', function(wordList) {
+            console.log(wordList);
+            callback(null, wordList.map(function(ea) {
+                return {
+                    name: ea.Word,
+                    value: ea.Word,
+                    score: ea.Count,
+                    meta: "tech"
+                }
+            }));
+        })
+
+        // $.getJSON(
+        //     "http://bar:3000/suggestion/" + prefix,
+        //     function(wordList) {
+        //         console.log(wordList)
+        //             // wordList like [{"word":"flow","freq":24,"score":300,"flags":"bc","syllables":"1"}]
+        //         callback(null, wordList.map(function(ea) {
+        //             return {
+        //                 name: ea.Word,
+        //                 value: ea.Word,
+        //                 score: ea.Count,
+        //                 meta: "tech"
+        //             }
+        //         }));
+        //     })
     }
-    langTools.addCompleter(rhymeCompleter);
+}
+langTools.addCompleter(rhymeCompleter);
 
-    editor.on('changeSession', function(e) {
-      // console.log('test');
-      console.log(editor.getSelectedText());
-    })
+editor.on('changeSession', function(e) {
+    // console.log('test');
+    console.log(editor.getSelectedText());
+})
 
-    document.getElementById('editor').addEventListener("dblclick", function(e) {
-      console.log(editor.getSelectedText());
-    });
+document.getElementById('editor').addEventListener("dblclick", function(e) {
+
+    console.log(editor.getSelectedText());
+});
